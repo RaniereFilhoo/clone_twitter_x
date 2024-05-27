701043e3dfa946a8820f686658b1609d21747c5a
Raniere Filho <filhoraniere7@gmail.com>
2024-05-27 00:12:26 -0300

    Criando tela de logout

--- projeto_x/clone_x_app/urls.py
+++ projeto_x/clone_x_app/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import home
+from .views import home, logout
 from . import views
 
 urlpatterns = [
     path('home', home, name='home'),
+    path('logout', logout, name='logout'),
     path('cadastro/', views.cadastro, name='cadastro'),
     path('login/', views.login_view, name='login'),
-    path('logout', views.login_view, name='logout'),
 ]
